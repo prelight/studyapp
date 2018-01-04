478801f4a3b38fca35f4a5f3144375c5d6c2fe05
Okamoto <prelight2006@yahoo.co.jp>
2018-01-04 18:38:18 +0900

    rnn first

---
+++ PythonApp/201801_week1/00_graph.py
@@ -0,0 +1,40 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main(): 
+
+    # ノイズありsin関数
+    ampl=0.05
+    T=100
+    x = np.arange(0, 2 * T + 1)#0から200までの連番
+    noise = ampl * np.random.uniform(low=-1.0, high=1.0, size=len(x))#疑似乱数
+    plt.plot(x, np.sin(2 * np.pi * x / T) + noise)
+    plt.show()
+
+
+    ## 指数関数
+    #x = np.arange(0, 10, 0.1)
+    #y = np.exp(x)
+    #plt.plot(x, y)
+    #plt.title("exponential function: $ y = e^x $")
+    #plt.ylim(0, 5000)
+    #plt.show()
+
+
+    ## sin関数
+    #pi = math.pi   #mathモジュールのπを利用
+    #x = np.linspace(0, 2*pi, 100)  #0から2πまでの範囲を100分割したnumpy配列
+    #y = np.sin(x)
+    #plt.plot(x, y)
+    #plt.show()
+
+
+    ## 折れ線グラフを出力
+    #left = np.array([1, 2, 3, 4, 5])
+    #height = np.array([100, 300, 200, 500, 400])
+    #plt.plot(left, height)
+    #plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ PythonApp/201801_week1/00_sin_simple_try.py
@@ -0,0 +1,47 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+def main(): 
+
+    def sin(x, T=100):
+        return np.sin(2.0 * np.pi * x / T)
+ 
+ 
+    def toy_problem(T=100, ampl=0.05):
+        x = np.arange(0, 2 * T + 1)
+        noise = ampl * np.random.uniform(low=-1.0, high=1.0, size=len(x))
+        return sin(x) + noise
+
+
+    T = 100
+    f = toy_problem(T)
+
+    length_of_sequences = 2 * T # 全時系列の長さ
+    maxlen = 25 # 1 つの時系列データの長さ
+ 
+    data = []
+    target = []
+ 
+    for i in range(0, length_of_sequences - maxlen + 1):
+        #print(i)
+        data.append(f[i: i + maxlen])
+        target.append(f[i + maxlen])
+    #print(data)
+    #print(maxlen)
+    #print(len(data))
+
+    #train_test_splitで使用できるようにreshape(?)
+    X = np.array(data).reshape(len(data), maxlen, 1)
+    Y = np.array(target).reshape(len(data), 1)
+
+    # データ設定
+    N_train = int(len(data) * 0.9)
+    N_validation = len(data) - N_train
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=N_validation)
+
+
+if __name__ == '__main__':
+    main()
