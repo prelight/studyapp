d79ca27d9473b27a4ec6d55e9b2358e4242a909d
Okamoto <prelight2006@yahoo.co.jp>
2018-02-08 19:14:45 +0900

    truncated_normal

--- PythonApp/201802_week2/temp_try.py
+++ PythonApp/201802_week2/temp_try.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import sys
+import tensorflow as tf
+
+
 
 #---------------------
 #①reshapeの意味
@@ -16,11 +19,18 @@ import sys
 #sys.exit()
 
 #---------------------
-#②参照渡し
-a = np.arange(0, 12)
-#a = range(0, 12)
-#b = np.reshape(copy.copy(a), (3, 4)) # 3×4の２次元配列に変形。
-b = np.reshape(a, (3, 4)) # 3×4の２次元配列に変形。
-b[2, 2] = 99
-print(a)
-print(b)
+##②参照渡し
+#a = np.arange(0, 12)
+##a = range(0, 12)
+##b = np.reshape(copy.copy(a), (3, 4)) # 3×4の２次元配列に変形。
+#b = np.reshape(a, (3, 4)) # 3×4の２次元配列に変形。
+#b[2, 2] = 99
+#print(a)
+#print(b)
+
+#---------------------
+#③truncated_normal
+x = tf.truncated_normal(shape=[2, 5],mean=0.0, stddev=1.0,dtype=tf.float32)
+with tf.Session() as sess:
+    y = x.eval()
+    print(y)
