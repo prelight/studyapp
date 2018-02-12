0d361121fe5903b3401b41aef497f187b2e6dac9
okamoto <prelight2006@yahoo.co.jp>
2018-02-13 07:24:38 +0900

    matomeマージ

--- PythonApp/201802_week2/book_dl_05_try.py
+++ PythonApp/201802_week2/book_dl_05_try.py
@@ -69,26 +69,82 @@ def main():
 
     X = np.array(data).reshape(len(data), maxlen, 1)
     #print(X)
-
     Y = np.array(target).reshape(len(data), 1)
-    
+
     
     #続き・・
-    ## データ設定
-    #N_train = int(len(data) * 0.9)
-    #N_validation = len(data) - N_train
+    # データ設定
+    N_train = int(len(data) * 0.9)
+    N_validation = len(data) - N_train
+    X_train, X_validation, Y_train, Y_validation = \
+        train_test_split(X, Y, test_size=N_validation)
 
-    #X_train, X_validation, Y_train, Y_validation = \
-    #    train_test_split(X, Y, test_size=N_validation)
+
+    '''
+    モデル設定
+    '''
+    n_in = len(X[0][0])  # 1
+    n_hidden = 20
+    n_out = len(Y[0])  # 1
+
+
+    def weight_variable(shape, name=None):
+        return np.random.normal(scale=.01, size=shape)
+
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+
+    model = Sequential()
+    model.add(SimpleRNN(n_hidden,
+                        kernel_initializer=weight_variable,
+                        input_shape=(maxlen, n_in)))
+    model.add(Dense(n_out, kernel_initializer=weight_variable))
+    model.add(Activation('linear'))
+
+    optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimizer)
 
     #'''
-    #モデル設定
+    #モデル学習
     #'''
-    #n_in = len(X[0][0])  # 1
-    #n_hidden = 20
-    #n_out = len(Y[0])  # 1
+    #epochs = 500
+    #batch_size = 10
 
+    #model.fit(X_train, Y_train,
+    #          batch_size=batch_size,
+    #          epochs=epochs,
+    #          validation_data=(X_validation, Y_validation),
+    #          callbacks=[early_stopping])
 
+    #'''
+    #出力を用いて予測
+    #'''
+    #truncate = maxlen
+    #Z = X[:1]  # 元データの最初の一部だけ切り出し
+
+    #original = [f[i] for i in range(maxlen)]
+    #predicted = [None for i in range(maxlen)]
+
+    #for i in range(length_of_sequences - maxlen + 1):
+    #    z_ = Z[-1:]
+    #    y_ = model.predict(z_)
+    #    sequence_ = np.concatenate(
+    #        (z_.reshape(maxlen, n_in)[1:], y_),
+    #        axis=0).reshape(1, maxlen, n_in)
+    #    Z = np.append(Z, sequence_, axis=0)
+    #    predicted.append(y_.reshape(-1))
+
+    #'''
+    #グラフで可視化
+    #'''
+    #plt.rc('font', family='serif')
+    #plt.figure()
+    #plt.ylim([-1.5, 1.5])
+    #plt.plot(toy_problem(T, ampl=0), linestyle='dotted', color='#aaaaaa')
+    #plt.plot(original, linestyle='dashed', color='black')
+    #plt.plot(predicted, color='black')
+    #plt.show()
 
 
 
