064808d259feafd2cad60d9363cf2ec45ebad844
Okamoto <prelight2006@yahoo.co.jp>
2018-02-28 17:23:41 +0900

    first_stock

--- PythonApp/201803_week1/first_stock.py
+++ PythonApp/201803_week1/first_stock.py
@@ -1,11 +1,34 @@
 import pandas as pd
 
-books = ['1321-10y.xlsx']
-df_list = []
+data = []
+target = []
 
-for book in books:
-    file = pd.ExcelFile(book) # bookを読む
-    for sheet in file.sheet_names:
-        df_list.append(file.parse(sheet)) # シートを順々にデータフレーム化
 
-print(df_list[0].values)
+def get_data():
+    books = ['1321-10y.xlsx']
+    candles = []
+
+    for book in books:
+        excel = pd.ExcelFile(book) # bookを読む
+        for sheet in excel.sheet_names:
+            if sheet == '1321-10y' :
+                sheetdf = excel.parse(sheet, header=None)
+
+    #column名、定義
+    cols = []
+    cols.append("time")
+    for i in range(10):
+        cols.append("c" + str(i))
+    cols.append("open")
+    cols.append("high")
+    cols.append("low")
+    cols.append("close")
+    sheetdf.columns = cols
+    return sheetdf
+
+df = get_data()
+
+
+print(df["close"])
+
+
