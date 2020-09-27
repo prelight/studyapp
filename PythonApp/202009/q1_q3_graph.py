abb5d94fb77efcf08045566f51fc7a35ec031d53
okamoto <prelight2006@yahoo.co.jp>
2020-09-27 10:05:48 +0900

    q1_q3 kerasで回答

---
+++ PythonApp/202009/q1_q3_graph.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def draw_graph(q_data, kinds, expected):
+    plt.rc('font', family='serif')
+    plt.figure()
+    # plt.ylim([0.5, 1.0])
+    #plt.plot(y_test, linestyle='dashed', color='red')#実際の値
+    # plt.plot(q_data, color='black')#予測値
+# x = "OK" if n == 10 else "NG"
+    for (dot, kind) in zip(q_data, kinds):
+        x = dot[0]
+        y = dot[1]
+        col = 'red' if kind[0] == 0  else 'black'
+
+        plt.plot(x, y, marker='.', color=col) 
+        plt.plot(expected[0], expected[1], marker='.', color='blue') 
+
+    
+
+
+    # plt.plot(1,1,marker='.')
+    # plt.plot(1,1,color='black')
+    plt.show()
+
+def main():
+    '''
+    モデル学習
+    '''
+    # テストデータ
+    X = np.array([[0, 1],[1, 2],[2, 3],[3, 2],[4, 9],[5, 0],[6, 10],[7, 9],[8, 12],[9, 6],[10, 8],
+                [11, 15],[12, 14],[13, 11],[14, 18],[15, 10],[16, 21],[17, 13],[18, 23],[19, 17]])
+    #Y = np.array([[0], [1], [1], [1]])
+    Y = np.array([[1],[1],[1],[0],[1],[0],[1],[1],[1],[0],[0],[1],[1],[0],[1],[0],[1],[0],[1],[0]])
+
+    Q = [20, 19]
+
+    draw_graph(X, Y, Q)
+    #get_answer_numpy()
+
+if __name__ == '__main__':
+    main()
+
+#交差エントロピーとは損失関数の１つ
+#train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)　→ 勾配降下法
+#sigmoid → 活性化関数
---
+++ PythonApp/202009/q1_q3_verification.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import SGD
+
+
+# テストデータ
+X = np.array([[0, 1],[1, 2],[2, 3],[3, 2],[4, 9],[5, 0],[6, 10],[7, 9],[8, 12],[9, 6],[10, 8],
+              [11, 15],[12, 14],[13, 11],[14, 18],[15, 10],[16, 21],[17, 13],[18, 23],[19, 17]])
+Y = np.array([[1],[1],[1],[0],[1],[0],[1],[1],[1],[0],[0],[1],[1],[0],[1],[0],[1],[0],[1],[0]])
+
+model = Sequential()
+model.add(Dense(input_dim=2, units=1))
+model.add(Activation('sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1)) #クロスエントロピー
+
+# 学習の実行
+model.ﬁt(X, Y, batch_size=1, epochs=200)
+
+# モデルの評価
+classes = model.predict_classes(np.array([[20, 19]]), batch_size=1) #クラス(カテゴリ)を返す
+prob = model.predict_proba(np.array([[20, 19]]), batch_size=1)      #クラス確率を返す
+
+print('classes=')
+print(classes)
+print('prob=')
+print(prob)
+
+#---------
+#交差エントロピーとは損失関数の１つ
+#train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)　→ 勾配降下法
+#sigmoid → 活性化関数
+#---------
+# 疑問
+# ・環境
+#     pylintがkeran_envに対応していない
+#     ターミナルがkeran_envになっていない
+#     コマンドプロンプトをデフォルトでkeran_envの環境にしたい
+# ・コード
+#     batch_sizeとは？
+#     input_dimって必要？　→ 検証結果：なくても行けたが・・
+#     既に学習したモデルを次回使いたいときは？
+
+
